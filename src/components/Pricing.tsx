c7358266407dc4b440f8c61a28feef0e474cc6dc
Sklearosi <giorgi.chanturidze.2000@gmail.com>
2023-08-30 19:57:31 +0400

    style:price component styles

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -17,10 +17,10 @@ const Pricing = () => {
         if (window.innerWidth < 768 ) {
          
           setStories(`mobile`)
-        } else if(window.innerWidth >= 768 && window.innerWidth < 1280) {
+        } else if(window.innerWidth >= 768 && window.innerWidth < 1080) {
          
           setStories(`desktop`)
-        } else if(window.innerWidth >= 1280 ){
+        } else if(window.innerWidth >= 1080 ){
             
             setStories(`desktop`)
         }
@@ -43,18 +43,18 @@ const Pricing = () => {
         <div>
             <div>
             <div className="md:grid md:grid-cols-3 md:h-featuresBegining">
-                <div style={{backgroundImage: `url(/assets/pricing/${stories}/hero.jpg)`}} className=" h-heroHeight bg-cover bg-center md:order-2 md:h-auto"></div>
-                <div className=" bg-black w-full h-blackDiv md:order-1 md:col-span-2 md:h-auto ">
+                <div style={{backgroundImage: `url(/assets/pricing/${stories}/hero.jpg)`}} className=" h-heroHeight bg-cover bg-center md:order-2 md:h-auto lg:col-span-2"></div>
+                <div className=" bg-black w-full h-blackDiv md:order-1 md:col-span-2 lg:col-span-1 md:h-auto ">
                 <div className=" md:w-10/12 m-auto md:mt-20">
-                    <h1 className="text-4xl font-bold leading-2 tracking-widest text-white uppercase pt-16 ml-6 md:text-6xl">PRICING</h1>
+                    <h1 className="text-4xl font-bold leading-2 tracking-widest text-white uppercase pt-16 ml-6 md:text-6xl lg:text-3xl">PRICING</h1>
                     <p className="text-base font-normal leading-5 text-white mt-9 ml-6 w-11/12">Create a your stories, Photosnap is a platform for photographers and visual storytellers. It’s the simple way to create and share your photos.</p>
                     </div>
                 </div>
                 </div>
             </div>
-            <div className=' flex w-64 justify-between items-center m-auto mt-12'>
+            <div className=' flex w-64 justify-between items-center m-auto mt-12 lg:pb-10'>
                 <p>Monthly</p>
-                <label htmlFor="pricing" className={`block rounded-3xl w-16 h-7 bg-lightGray relative`}>
+                <label htmlFor="pricing" className={` cursor-pointer block rounded-3xl w-16 h-7 bg-lightGray relative`}>
                     <div className={`w-6 h-6 bg-black rounded-full absolute  transition-all duration-500 ${!isChecked ? " ml-1" : "ml-9"}`}></div>
                 </label>
                 <input type="checkbox" id="pricing" className="hidden" onChange={() => {
@@ -75,48 +75,48 @@ const Pricing = () => {
                 }}/>
                 <p>Yearly</p>
             </div>
-            <div className='w-11/12 m-auto mt-12 grid gap-10'>
-                <div className={` bg-lightGray  text-black text-center pb-14 pt-7 md:h-heroHeight md:pb-0 md:pt-0 md:relative `}>
-                    <div className=' md:relative md:top-6   md:w-11/12 md:m-auto '>
-                <div className=' w-10/12 m-auto md:text-left md:w-64 md:left-0 md:m-0'>
+            <div className='w-11/12 m-auto mt-12 grid gap-10 lg:grid-cols-3 lg:pb-10'>
+                <div className={` bg-lightGray  text-black text-center pb-14 pt-7 md:h-heroHeight md:pb-0 md:pt-0 md:relative  lg:h-[407px]`}>
+                    <div className=' md:relative md:top-6   md:w-11/12 md:m-auto lg:static'>
+                <div className=' w-10/12 m-auto md:text-left md:w-64 md:left-0 md:m-0 lg:text-center lg:w-full lg:mt-10'>
                     <p className='pt-10 text-2xl font-bold leading-6'>Basic</p>
                     <p className='mt-5 text-base font-normal leading-5'>Includes basic usage of our platform. Recommended for new and aspiring photographers.</p>
                     </div>
-                    <div className='md:absolute md:right-0 md:top-2'>
+                    <div className='md:absolute md:right-0 md:top-2 lg:bottom-22 lg:top-[unset] lg:left-0 lg:right-0 lg:m-auto'>
                     <p className='text-4xl font-bold leading-12 tracking-[4.166666507720947px] mt-8 md:text-3xl'>{price.basic}</p>
                     <p className='mt-2'>per month</p>
                     </div>
-                    <button className='text-white bg-black w-60 h-10 text-center block m-auto mt-10 md:absolute md:left-0 md:m-0  md:bottom-0 md:top-44'>PICK PLAN</button>
+                    <button className='hover:bg-white hover:text-black transition-all duration-500 text-white bg-black w-60 h-10 text-center block m-auto mt-10 md:absolute md:left-0 md:m-0  md:bottom-0 md:top-44 lg:bottom-12 lg:top-[unset] lg:left-0 lg:right-0 lg:m-auto'>PICK PLAN</button>
                     </div>
                 </div>
-                <div className={` bg-black  text-white text-center pb-14 pt-7 md:h-heroHeight md:pb-0 md:pt-0 md:relative `}>
-                    <div className=' md:relative md:top-6   md:w-11/12 md:m-auto '>
-                <div className=' w-10/12 m-auto md:text-left md:w-64 md:left-0 md:m-0'>
+                <div className={` bg-black  text-white text-center pb-14 pt-7 md:h-heroHeight md:pb-0 md:pt-0 md:relative  lg:h-[470px] lg:-mt-5`}>
+                    <div className=' md:relative md:top-6   md:w-11/12 md:m-auto lg:static'>
+                <div className=' w-10/12 m-auto md:text-left md:w-64 md:left-0 md:m-0 lg:text-center lg:w-full lg:mt-10'>
                     <p className='pt-10 text-2xl font-bold leading-6'>Basic</p>
                     <p className='mt-5 text-base font-normal leading-5'>Includes basic usage of our platform. Recommended for new and aspiring photographers.</p>
                     </div>
-                    <div className='md:absolute md:right-0 md:top-2'>
-                    <p className='text-4xl font-bold leading-12 tracking-[4.166666507720947px] mt-8 md:text-3xl'>{price.pro}</p>
+                    <div className='md:absolute md:right-0 md:top-2 lg:bottom-22 lg:top-[unset] lg:left-0 lg:right-0 lg:m-auto'>
+                    <p className='text-4xl font-bold leading-12 tracking-[4.166666507720947px] mt-8 md:text-3xl'>{price.basic}</p>
                     <p className='mt-2'>per month</p>
                     </div>
-                    <button className='text-black bg-white w-60 h-10 text-center block m-auto mt-10 md:absolute md:left-0 md:m-0  md:bottom-0 md:top-44'>PICK PLAN</button>
+                    <button className='hover:bg-lightGray transition-all duration-500 text-black bg-white w-60 h-10 text-center block m-auto mt-10 md:absolute md:left-0 md:m-0  md:bottom-0 md:top-44 lg:bottom-12 lg:top-[unset] lg:left-0 lg:right-0 lg:m-auto'>PICK PLAN</button>
                     </div>
                 </div>
-                <div className={` bg-lightGray  text-black text-center pb-14 pt-7 md:h-heroHeight md:pb-0 md:pt-0 md:relative `}>
-                    <div className=' md:relative md:top-6   md:w-11/12 md:m-auto '>
-                <div className=' w-10/12 m-auto md:text-left md:w-64 md:left-0 md:m-0'>
+                <div className={` bg-lightGray  text-black text-center pb-14 pt-7 md:h-heroHeight md:pb-0 md:pt-0 md:relative  lg:h-[407px]`}>
+                    <div className=' md:relative md:top-6   md:w-11/12 md:m-auto lg:static'>
+                <div className=' w-10/12 m-auto md:text-left md:w-64 md:left-0 md:m-0 lg:text-center lg:w-full lg:mt-10'>
                     <p className='pt-10 text-2xl font-bold leading-6'>Basic</p>
                     <p className='mt-5 text-base font-normal leading-5'>Includes basic usage of our platform. Recommended for new and aspiring photographers.</p>
                     </div>
-                    <div className='md:absolute md:right-0 md:top-2'>
-                    <p className='text-4xl font-bold leading-12 tracking-[4.166666507720947px] mt-8 md:text-3xl'>{price.business}</p>
+                    <div className='md:absolute md:right-0 md:top-2 lg:bottom-22 lg:top-[unset] lg:left-0 lg:right-0 lg:m-auto'>
+                    <p className='text-4xl font-bold leading-12 tracking-[4.166666507720947px] mt-8 md:text-3xl'>{price.basic}</p>
                     <p className='mt-2'>per month</p>
                     </div>
-                    <button className='text-white bg-black w-60 h-10 text-center block m-auto mt-10 md:absolute md:left-0 md:m-0  md:bottom-0 md:top-44'>PICK PLAN</button>
+                    <button className='hover:bg-white hover:text-black transition-all duration-500 text-white bg-black w-60 h-10 text-center block m-auto mt-10 md:absolute md:left-0 md:m-0  md:bottom-0 md:top-44 lg:bottom-12 lg:top-[unset] lg:left-0 lg:right-0 lg:m-auto'>PICK PLAN</button>
                     </div>
                 </div>
             </div>
-            <div className=' w-11/12 m-auto mt-10 pb-14'>
+            <div className=' w-11/12 m-auto mt-10 pb-14 lg:w-7/12'>
                 <div className='flex justify-between'>
                 <p className='mb-4'>THE FEATURES</p>
                 <div className='hidden md:flex  md:w-80 md:mr-16  md:justify-between'><p>BASIC</p><p>PRO</p><p>BUSINESS</p></div>
